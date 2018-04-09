99d2138b2bd08c16064f014e3c147dfa27cda3e3
Michael White <michael@esprofessionals.com>
2018-04-09 20:54:04 +0800

    fix nooo.mp3 path

--- cli/commands/nooo.js
+++ cli/commands/nooo.js
@@ -3,7 +3,7 @@ var path = require('path');
 function nooo () {
     var player = require('play-sound')(opts = {});
 
-    player.play(path.resolve(__dirname, 'nooo.mp3'), function(err){
+    player.play(path.resolve(__dirname, '../nooo.mp3'), function(err){
     if (err) throw err
   });
 }
